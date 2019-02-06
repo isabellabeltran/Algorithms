e6a08dad87af67633485a462d66805e16518df4c
Isabella Beltran <isabella-beltran@pluralsight.com>
2019-02-06 11:23:45 -0500

    Add a first approach to solve the first daily problem

--- src/array.js
+++ src/array.js
@@ -35,6 +35,32 @@ const twoSumLinear = (arr, int) => {
   }
 }
 
+/**
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+  For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+  First approach:
+    iterate over the array and for each number 
+      if the number is less than k 
+        iterate over the rest of the numbers and for each number
+          add them to i and if they are equal to k return true 
+    if there is no matching sum return false   
+ */
+
+
+const addNumber = (arr, int) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (i < int) {
+      for (let j = 0; j < arr.length; j++) {
+        if (i + j === int) {
+          return true;
+        }
+      }
+    }
+  }
+  return false; 
+}
+
 module.exports = {
   twoSum,
   twoSumLinear
